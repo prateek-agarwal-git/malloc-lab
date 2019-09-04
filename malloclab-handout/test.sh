36c538d810eaaf9f8d64ab6e2003a0e6eae5d6ea
PRATEEK AGARWAL <prateekag@cse.iitb.ac.in>
2019-09-04 11:35:00 +0530

    selfnotes updated with implementation details

---
+++ malloclab-handout/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+./mdriver -V -f short1-bal.rep
+./mdriver -V -f amptjp-bal.rep
+./mdriver -V -f cccp-bal.rep
+./mdriver -V -f expr-bal.rep
+./mdriver -V -f coalescing-bal.rep
+./mdriver -V -f random-bal.rep
+./mdriver -V -f random2-bal.rep
+./mdriver -V -f binary-bal.rep
+./mdriver -V -f binary2-bal.rep
+./mdriver -V -f realloc-bal.rep
+./mdriver -V -f binary2-bal.rep
+./mdriver -V -f realloc2-bal.rep
+
---
+++ malloclab-handout/test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int * a;
+	printf("\n");
+	printf("%lu", sizeof(a));
+	printf("\n");
+	return 0;
+}
+
