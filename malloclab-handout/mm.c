4b7c5e8b95ff730c9e543fbd639d80dbe29477a3
PRATEEK AGARWAL <prateekag@cse.iitb.ac.in>
2019-09-08 23:21:32 +0530

    memcmp

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -16,10 +17,7 @@ team_t team = {
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 #define GETHEADER(bp) (*bp) 
-#define GETSIZEHEADER(bp) (*(size_t *)bp)
-//#define GETPREVFREE(bp) *(size_t  *) (bp+SIZE_T_SIZE)
-//#define GETNEXTFREE(bp) *(char *) ( bp+SIZE_T_SIZE+ALIGN(sizeof(char *)))
-//#define GETFOOTER(bp)((bp + GETSIZEHEADER(bp)- SIZE_T_SIZE))
+#define GETSIZEHEADER(bp) (*(unsigned int  *)bp)
 #define MINSIZE (2*SIZE_T_SIZE +2* ALIGN(sizeof(void *)))
 #define IS_ALIGNED(p)  ((((unsigned int)(p)) % ALIGNMENT) == 0)
 static char * mm_heap;
@@ -31,66 +29,57 @@ void * mm_malloc(size_t);
 void mm_free(void *);
 void *mm_realloc(void *, size_t);
 void  delete_from_freelist(void * bp);
-void  setnextfree(void * , void * );
-
+void  setnextfree(void ** , void * );
+void  setprevfree(void ** , void * );
 void printfreelist(){
 	void * curr ;
-	setnextfree(curr,mm_head);
-	//printf("free size%u", GETSIZEHEADER(curr));
-	//printf("Raja\n");
+	setnextfree(&curr,mm_head);
 	while (curr!= NULL){
-		//assert(curr!= NULL);
-		printf("free size%u", GETSIZEHEADER(curr));
+//		printf("free size%u", GETSIZEHEADER(curr));
 		void * temp;
-		setnextfree(temp,curr);
+		setnextfree(&temp,curr);
 		curr = temp;
+
 	}
 	return;
+		//printf("is curr null %u\n",curr);
 
 }
 int mm_init(void)
 {
-	//make a dummy head node for free list
 	mem_init();
 	size_t minsize = MINSIZE+1;
 	mm_head =mem_sbrk(MINSIZE);
-//	assert(!IS_ALIGNED(mm_head));
-	mm_heap = mm_head;
+	//mm_heap = mm_head+MINSIZE-1;
 	memcpy(mm_head,&minsize,sizeof(size_t));//header
 	memcpy(mm_head+MINSIZE-SIZE_T_SIZE,&minsize, sizeof(size_t));//footer
 	memset(mm_head + SIZE_T_SIZE, 0, sizeof(void *));//pre
 	memset(mm_head + SIZE_T_SIZE+ALIGN(sizeof(void *)),0, sizeof(void *));//next
-	//printf("init getsizeheader %u",GETSIZEHEADER(mm_head));
-
 	return 0;
 }
 
 void *mm_malloc(size_t payload)
-{	printfreelist();
+{	//printfreelist();
 	size_t size = ALIGN(payload)+2*SIZE_T_SIZE;
 	if (size<MINSIZE) size  = MINSIZE;
 	void * block_pointer;
-	block_pointer =search_free_list(size); 
+	block_pointer =search_free_list(size);
+ 	//assert(block_pointer != NULL);
 	if (block_pointer!= NULL) return block_pointer;	
 	block_pointer = mem_sbrk(size);
 	if (block_pointer == (void *) -1){
 		assert(3==4);
 		 return NULL;}
+	assert(mm_head!=block_pointer); 
 	mm_heap = block_pointer;
 	size = size|0x1;
-	assert(IS_ALIGNED(block_pointer));
-//	assert
-	//assert(size%2==0);
-	//size is odd
 	memcpy(block_pointer, &size, sizeof(size_t));
 	size_t step = size&~0x1;
-	assert(IS_ALIGNED(block_pointer+step));
 	memcpy(block_pointer+step-SIZE_T_SIZE, &size, sizeof(size_t));
-	//printf(" hi %u ", GETSIZEHEADER(block_pointer));
 	return block_pointer;
 }
 void mm_free(void *bp){
-	//printfreelist();
+	void * temp;
 	if (bp==mm_heap){
 		size_t physicalprevsize =*(size_t *) ( bp - SIZE_T_SIZE);
 		size_t physicalprevbool = physicalprevsize & 0x1;
@@ -100,24 +89,23 @@ void mm_free(void *bp){
 			memcpy(bp, &currsize, sizeof(size_t));
 			memcpy(bp +(currsize - SIZE_T_SIZE), &currsize, sizeof(size_t));
 			insertfreelist(bp,mm_head);
-			return;//both are allocated
+			printfreelist();
+			return;
 		}
 		else{
 			size_t newfreesize = (GETSIZEHEADER(bp)&~0x1) +(physicalprevsize &~0x1); 
 			assert(newfreesize %ALIGNMENT == 0);
 			delete_from_freelist((char *)bp-physicalprevsize);
-//			assert(IS_ALIGNED(bp-physicalprevsize));
 			memcpy(bp -physicalprevsize, &newfreesize, sizeof(size_t));
 			assert(IS_ALIGNED(bp +GETSIZEHEADER(bp)));
 			size_t temp =GETSIZEHEADER(bp)&~0x1; 
 			memcpy(bp+ temp- SIZE_T_SIZE, &newfreesize, sizeof(size_t));
-			insertfreelist(bp-physicalprevsize,mm_head);
+			void * l =bp-physicalprevsize;
+			insertfreelist(l,mm_head);
 			return;
 		}
 	}
-	else{
-		
-		size_t physicalprevsize =*(size_t *) ( bp - SIZE_T_SIZE);
+	else{	size_t physicalprevsize =*(size_t *) ( bp - SIZE_T_SIZE);
 		size_t physicalnextsize = *(size_t *)(bp +(GETSIZEHEADER(bp)&~0x1));
 		size_t 	physicalprevbool = physicalprevsize & 0x1;
 		size_t physicalnextbool = physicalnextsize &  0x1;
@@ -126,7 +114,7 @@ void mm_free(void *bp){
 			memcpy(bp, &currsize, sizeof(size_t));
 			memcpy(bp +currsize - SIZE_T_SIZE, &currsize, sizeof(size_t));
 			insertfreelist(bp,mm_head);
-			return;//both are allocated
+			return;
 		}
 		else if (physicalprevbool && !physicalnextbool){//next is free
 			size_t newfreesize = GETSIZEHEADER(bp) +(physicalnextsize &~0x1);
@@ -195,8 +183,7 @@ void *mm_realloc(void *ptr, size_t payload)
 		}
 		return ptr;
 	}	
-	else{
-		void * newptr = mm_malloc(size);
+	else{	void * newptr = mm_malloc(size);
 		if (newptr == NULL){
 		return  NULL;
 		}
@@ -204,28 +191,32 @@ void *mm_realloc(void *ptr, size_t payload)
 		mm_free(ptr);
 		return newptr;
 	}  
-  	return ptr;
-}
+  	return ptr;}
 void * search_free_list(size_t size){
 	void * curr ;
-	setnextfree(curr,mm_head);
+	setnextfree(&curr,mm_head);
 	int i = 0;
-	assert((size>=MINSIZE) &&(size%ALIGNMENT == 0));
+	//assert((size>=MINSIZE) &&(size%ALIGNMENT == 0));
 	while(curr!= NULL){
-		
-		if (GETSIZEHEADER(curr)>= size){
+		printf(" \nwhich free blocks(2056)  %u what size(3040-3056) %u\n ",GETSIZEHEADER(curr), size );
+
+		if (memcmp(curr, &size,sizeof(size_t))>=0){
+			printf("hi what is up\n");
 			size_t freeblocksize = GETSIZEHEADER(curr);
-			assert(freeblocksize%ALIGNMENT == 0);
+			assert(1==2);
+			//if (freeblocksize < size){
+			//	return NULL;
+			//}
 			if (freeblocksize - size >= MINSIZE){
 				void * prev;
 				void * next;
-				setnextfree(next,curr);
-				setprevfree(prev,free);
+				setnextfree(&next,curr);
+				setprevfree(&prev,free);
 				size = size|0x1;
 				size_t step = size &~0x1;								
 				memcpy(curr, &size, sizeof(size_t));
 				memcpy(curr+ step - SIZE_T_SIZE, &size,sizeof(size_t));
-				size_t remaining = step - freeblocksize;
+				size_t remaining =  freeblocksize - step;
 				assert(remaining%ALIGNMENT == 0);
 				memcpy(curr+step, &remaining, sizeof(size_t));
 				memcpy(curr+ freeblocksize - SIZE_T_SIZE, &remaining,sizeof(size_t));
@@ -238,46 +229,52 @@ void * search_free_list(size_t size){
 				memcpy(curr, &size, sizeof(size_t));
 				memcpy(curr+ step - SIZE_T_SIZE, &size,sizeof(size_t));
 				return curr;
-
 			}
 		}
 		void * temp;
-		setnextfree(temp,curr);
+		setnextfree(&temp,curr);
 		curr = temp; 
 		i++;
 	}
 	return NULL;	
 }
-void  setnextfree(void * dest, void * src){
+void  setnextfree(void ** dest, void * src){
 	size_t sourcesize = GETSIZEHEADER(src);
 	sourcesize = sourcesize &~0x1;
-	memcpy(&dest,src+SIZE_T_SIZE+ALIGN(sizeof(void *)),sizeof(void *));// store next of prev in a temp variable	
+	memcpy(dest,src+SIZE_T_SIZE+ALIGN(sizeof(void *)),sizeof(void *));// store next of prev in a temp variable
+	//printf("inside setnextfree %u ",*dest);	
 	return;
 }
-void  setprevfree(void * dest, void * src){
+void  setprevfree(void ** dest, void * src){
 	size_t sourcesize = GETSIZEHEADER(src);
 	sourcesize = sourcesize &~0x1;
-	memcpy(&dest,src+SIZE_T_SIZE,sizeof(void *));// store next of prev in a temp variable	
+	memcpy(dest,src+SIZE_T_SIZE,sizeof(void *));// store next of prev in a temp variable	
 	return;
 }
 
 void insertfreelist(void * bp, void *prev){
 	void *nexttohead;
 	size_t headsize = GETSIZEHEADER(prev);
-	setnextfree(nexttohead,prev);
+	setnextfree(&nexttohead,prev);
 	if (nexttohead != NULL){
-		memcpy(bp + SIZE_T_SIZE, &prev, sizeof(void *) );
+		memcpy(bp + SIZE_T_SIZE,&prev, sizeof(void *) );
 		memcpy(bp + SIZE_T_SIZE+ALIGN(sizeof(void *)),&nexttohead, sizeof(void *) );
 		memcpy(prev+SIZE_T_SIZE + ALIGN(sizeof(void *)),&bp, sizeof(void *));
 		void * previousnexttohead;
-		setnextfree(previousnexttohead, bp);
+		setnextfree(&previousnexttohead, bp);
 		memcpy(previousnexttohead + SIZE_T_SIZE, &bp, sizeof(void *));
 		return;
 	}
 	else{
+		//printf("\nbp should not be zero %u\n", bp);
+		//printf("before memcpy %u\n", prev+SIZE_T_SIZE+ALIGN(sizeof(void *)));
+  		memcpy(prev+SIZE_T_SIZE+ALIGN(sizeof(void *)),&bp, sizeof(void *));
+		//printf("checking memcpy? %u\n", *(void **)(prev+SIZE_T_SIZE + ALIGN(sizeof(void *))));
+		//void * temp2;
+		//setnextfree(&temp2,prev);
+		//printf("\nwhat here bp?? %u\n",temp2 );
 		memcpy(bp+SIZE_T_SIZE,&prev,sizeof(void *) );
 		memset(bp+SIZE_T_SIZE+ ALIGN(sizeof(void *)),0,sizeof(void *) );
-		memcpy(prev+SIZE_T_SIZE+ALIGN(sizeof(void *)),&bp, sizeof(void *));
 		return;
 	}
 }
@@ -286,40 +283,14 @@ void insertfreelist(void * bp, void *prev){
 void  delete_from_freelist(void * bp){
 	void * next;
 	void * prev;
-	setnextfree(next, bp);
-	setprevfree(prev,bp);
+	setnextfree(&next, bp);
+	setprevfree(&prev,bp);
 	if (next!= NULL){
-		memcpy((char *)prev + SIZE_T_SIZE + ALIGN(sizeof(void *)), &next, sizeof (void *));
-		memcpy((char *)next + SIZE_T_SIZE , &prev, sizeof (void *));
+		memcpy(prev + SIZE_T_SIZE + ALIGN(sizeof(void *)), &next, sizeof (void *));
+		memcpy(next + SIZE_T_SIZE , &prev, sizeof (void *));
 	}
-	else{
-		
-		memset((char *)prev + SIZE_T_SIZE + ALIGN(sizeof(void *)), 0, sizeof (void *));
-	//	memcpy((char *)prev + SIZE_T_SIZE + ALIGN(sizeof(void *)), &next, sizeof (void *));
+	else memset(prev + SIZE_T_SIZE + ALIGN(sizeof(void *)), 0, sizeof (void *));
 
-
-	}
+	
 	return;
 } 
-
-
-
-	/*else{
-		update size with freeblocksize;
-		allocate the whole block and return pointer;
-		}*/
-
-//searches the free list when malloc is called.
-//steps: 1) search a block with the given size
-
-//2) if found: 
-/*  check size of the free block.
-new headers and footers and allocate bit should be set to 1.
-// now some free space is remaining. if it is greater than min size:
-//set header and footer of the remaining block and 
-*/
-// else: remove the block from free list
-//
-//
-//
-//
